6dbd94f34b07e8fbaa0b9424e791c59e9cf1305d
squentel <30870132+squentel@users.noreply.github.com>
2021-03-02 11:26:35 +0100

    Update ChromeSimon.ps1

--- ChromeSimon.ps1
+++ ChromeSimon.ps1
@@ -1,6 +1,6 @@
 $pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 $url1 = "http://172.17.0.21:8081"
-$url2 = "https://www.google.com/"
+$url2 = "https://www.salesforce.com/"
 
 Start-Process -FilePath $pathToChrome -ArgumentList $url1, --no-sandbox
 Start-Process -FilePath $pathToChrome -ArgumentList $url2, --no-sandbox
