0ba2e2e033ab48a3dfeb79f55407420002b528a1
squentel <30870132+squentel@users.noreply.github.com>
2021-04-16 12:45:37 +0200

    Update ChromeURL.ps1

--- ChromeURL.ps1
+++ ChromeURL.ps1
@@ -1,5 +1,5 @@
 $pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-$url1 = "http://www.1passsword.com/"
+$url1 = "http://www.1passsword.com:8081/"
 $url2 = "https://www.salesforce.com/"
 
 Start-Process -FilePath $pathToChrome -ArgumentList $url1, --no-sandbox
