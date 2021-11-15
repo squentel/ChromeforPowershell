37d130a74a32aba123101361bd6621d295d90ed2
squentel <30870132+squentel@users.noreply.github.com>
2021-11-15 15:57:53 +0100

    Create appfile.ps1

---
+++ appfile.ps1
@@ -0,0 +1,7 @@
+$listApp=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
+$listApps | Out-File -filepath C:\myapps.txt
+$file = "C:\myapps.txt"
+$ftpuri = "ftp://simon:QuenteL789!@172.17.0.34/myapps2.txt"
+$webclient = New-Object System.Net.WebClient
+$uri = New-Object System.Uri($ftpuri)
+$webclient.UploadFile($uri, $file) 
