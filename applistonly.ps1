59b312cee5de9885d087abdc9ccf8db32e7a67b6
squentel <30870132+squentel@users.noreply.github.com>
2021-11-18 16:55:26 +0100

    Update applistonly.ps1

--- applistonly.ps1
+++ applistonly.ps1
@@ -1,7 +1,8 @@
 $listApps=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
 $listApps | Out-File -filepath C:\myapps.txt
+$FileNameTS = (Get-Date).tostring("dd-MM-yyyy-hh-mm-ss");
 $file = "C:\myapps.txt"
-$ftpuri = "ftp://demoUser:CrowdDemo789!@172.17.0.34/myapps.txt"
+$ftpuri = "ftp://demoUser:CrowdDemo789!@172.17.0.34/myapps"+$FileNameTS+".txt"
 $webclient = New-Object System.Net.WebClient
 $uri = New-Object System.Uri($ftpuri)
 $webclient.UploadFile($uri, $file)  
