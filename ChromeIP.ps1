3c9bffd8d4a7c8c324e7801c361a07b13c40acf8
squentel <30870132+squentel@users.noreply.github.com>
2024-02-26 14:00:40 +0100

    Update ChromeIP.ps1

--- ChromeIP.ps1
+++ ChromeIP.ps1
@@ -1,5 +1,5 @@
 $pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-$url1 = "http://10.3.158.21:8080"
+$url1 = "http://www.1-password.com:8080"
 $url2 = "https://www.salesforce.com/"
 
 Start-Process -FilePath $pathToChrome -ArgumentList $url1, --no-sandbox
