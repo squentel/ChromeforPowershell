d873b1430f354428c31c48bf4488f405c8ea61ef
squentel <30870132+squentel@users.noreply.github.com>
2024-02-29 09:15:25 +0100

    Update ChromeEncounter.ps1

--- ChromeEncounter.ps1
+++ ChromeEncounter.ps1
@@ -1,5 +1,5 @@
 $pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-$url1 = "http://www.1passsword.com:80"
+$url1 = "http://www.1passsword.com:8080"
 $url2 = "https://www.salesforce.com/"
 
 Start-Process -FilePath $pathToChrome -ArgumentList $url1, --no-sandbox
