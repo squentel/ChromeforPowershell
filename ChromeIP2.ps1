e45640cce3311d7ba44b22ddb0d36881e1cc1151
squentel <30870132+squentel@users.noreply.github.com>
2024-03-06 09:39:57 +0100

    Create ChromeIP2.ps1

---
+++ ChromeIP2.ps1
@@ -0,0 +1,7 @@
+$pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+$url1 = "http://www.1-passsword.com:8080"
+$url2 = "https://www.salesforce.com/"
+
+Start-Process -FilePath $pathToChrome -ArgumentList $url1, --no-sandbox
+Start-Sleep -s 2
+Start-Process -FilePath $pathToChrome -ArgumentList $url2, --no-sandbox
